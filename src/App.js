539537cc2425a6de3dd2fa813c82e32bebebc42a
ardaorkin <ardaorkin@ardaorkin>
2020-05-02 02:46:16 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+
+  mapboxgl.accessToken = 'pk.eyJ1IjoiYXJkYW9ya2luIiwiYSI6ImNrOW9teW8wMzAyNnczbHJ0emVvNHE5dXcifQ.J_P9VwfH6UeYpgG5gw-JJQ';
+
+  React.useEffect(() => {
+    var map = new mapboxgl.Map({
+      container: 'root',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      zoom: 3
+    });
+
+
+    map.on('click', (e) => {
+      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${e.lngLat.lng.toString()},${e.lngLat.lat.toString()}.json?&access_token=${mapboxgl.accessToken}`, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json'
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        }
+      })
+      .then(res => 
+        res.json()
+      )
+      .then(result => {
+        result.features.map(feature => {
+          if(feature.place_type[0] === "country") {
+            console.log(feature.place_name)
+          }
+        })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    })
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="map" className="App">
+      
     </div>
   );
 }
