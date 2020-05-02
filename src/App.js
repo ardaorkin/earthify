1f89fb98994c53517411368e49933edd89e546f6
ardaorkin <ardaorkin@ardaorkin>
2020-05-02 16:34:29 +0300

    talk to spotify-api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import './App.css';
 
 
 function App() {
+  const isAuth = window.localStorage.getItem('auth') || false
+  const [auth, setAuth] = React.useState(isAuth)
+  var baseURI = "https://api.spotify.com/"
+  var client_id = '9e71a4da3ee24d31ab4fd842607cce9e'; // Your client id
+  var redirect_uri = window.location.origin + "/callback"; // Your redirect uri
+  var scopes = 'user-read-private user-read-email';
+
+
   var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiYXJkYW9ya2luIiwiYSI6ImNrOW9teW8wMzAyNnczbHJ0emVvNHE5dXcifQ.J_P9VwfH6UeYpgG5gw-JJQ';
@@ -16,32 +24,49 @@ function App() {
 
 
     map.on('click', (e) => {
-      fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${e.lngLat.lng.toString()},${e.lngLat.lat.toString()}.json?&access_token=${mapboxgl.accessToken}`, {
-        method: "GET",
-        headers: {
-          'Content-Type': 'application/json'
-          // 'Content-Type': 'application/x-www-form-urlencoded',
-        }
-      })
-      .then(res => 
-        res.json()
-      )
-      .then(result => {
-        result.features.map(feature => {
-          if(feature.place_type[0] === "country") {
-            console.log(feature.place_name)
+      if (!window.location.hash && !auth) {
+        fetch("https://accounts.spotify.com/authorize?client_id=" + client_id + "&response_type=token" + "&redirect_uri=" + encodeURIComponent(redirect_uri) + "&scope=" + encodeURIComponent(scopes) + "&show_dialog=true", {
+          method: "GET"
+        })
+          .then((res) => {
+            window.location = res.url
+          })
+      }
+      else {
+        window.localStorage.setItem('auth', true)
+        fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${e.lngLat.lng.toString()},${e.lngLat.lat.toString()}.json?&access_token=${mapboxgl.accessToken}`, {
+          method: "GET",
+          headers: {
+            'Content-Type': 'application/json'
           }
         })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+          .then(res =>
+            res.json()
+          )
+          .then(result => {
+            result.features.map(feature => {
+              if (feature.place_type[0] === "country") {
+                console.log(feature.place_name)
+              }
+            })
+          })
+          .catch((err) => {
+            console.log(err)
+          })
+      }
     })
+    if(window.location.hash) {
+      var hash = window.location.hash.substr(1); //url of the current page
+      var access_token = hash.split('=')[1].split('&')[0]; //this creates an array with key ([0] element) and value ([1] element)
+      console.log(access_token)
+    }
   }, [])
   return (
-    <div id="map" className="App">
-      
-    </div>
+    <>
+      <div id="map" className="App">
+
+      </div>
+    </>
   );
 }
 
