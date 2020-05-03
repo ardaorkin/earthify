db3c239a44f2c749b6cd0da61af12cf6f628727c
ardaorkin <ardaorkin@ardaorkin>
2020-05-03 22:08:53 +0300

    Updates

--- precache-manifest.c2d248b25543e04f289a0e80310a2566.js
+++ precache-manifest.c2d248b25543e04f289a0e80310a2566.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1e75554d259e36754e10a5b693b42c6c",
+    "revision": "7ebf0ea48c604da6b7b5137a4fe12faa",
     "url": "/earthify/index.html"
   },
   {
-    "revision": "fcd8cbc6b80c4b3c5ef4",
+    "revision": "81a3e184fd6323e3c874",
     "url": "/earthify/static/css/main.d2a4fabb.chunk.css"
   },
   {
-    "revision": "dd04379f73d6bf781f54",
-    "url": "/earthify/static/js/2.33154ecd.chunk.js"
+    "revision": "6beb952c71f8ede07edb",
+    "url": "/earthify/static/js/2.3004b986.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/earthify/static/js/2.33154ecd.chunk.js.LICENSE.txt"
+    "url": "/earthify/static/js/2.3004b986.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "fcd8cbc6b80c4b3c5ef4",
-    "url": "/earthify/static/js/main.8b260f9e.chunk.js"
+    "revision": "81a3e184fd6323e3c874",
+    "url": "/earthify/static/js/main.42cbfb2f.chunk.js"
   },
   {
     "revision": "545d5ac5909b7cb1537c",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/earthify/precache-manifest.5a642076fcb14329e9edc0e5ee85347e.js"
+  "/earthify/precache-manifest.c2d248b25543e04f289a0e80310a2566.js"
 );
 
 self.addEventListener('message', (event) => {
