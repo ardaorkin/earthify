04b44b92cca9723a94b1eeff1d2a61a1f4ca528c
ardaorkin <ardaorkin@ardaorkin>
2020-05-03 16:39:40 +0300

    play onclick

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const [accessToken, setToken] = React.useState(localStorage.getItem('access_token'))
   const isAuth = window.localStorage.getItem('auth') || false
   const [auth, setAuth] = React.useState(isAuth)
   var baseURI = "https://api.spotify.com/"
   var client_id = '9e71a4da3ee24d31ab4fd842607cce9e';
   var redirect_uri = window.location.origin + window.location.pathname
-  var scopes = 'user-modify-playback-state';
+  var scopes = 'user-modify-playback-state user-read-playback-state';
 
 
   var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
   mapboxgl.accessToken = 'pk.eyJ1IjoiYXJkYW9ya2luIiwiYSI6ImNrOW9teW8wMzAyNnczbHJ0emVvNHE5dXcifQ.J_P9VwfH6UeYpgG5gw-JJQ';
+
+  if (window.location.hash.match(/access_token/g) !== null) {
+    async function afterAuthorize() {
+      await window.localStorage.setItem('auth', true)
+      var hash = window.location.hash.substr(1);
+      localStorage.setItem('access_token', hash.split('=')[1].split('&')[0])
+      window.location = window.location.origin
+    }
+    afterAuthorize()
+  }
+
 
   React.useEffect(() => {
     var map = new mapboxgl.Map({
@@ -21,14 +33,13 @@ function App() {
       zoom: 3
     });
 
+
+
     map.on('click', (e) => {
       if (!window.location.hash && !auth) {
         window.location = "https://accounts.spotify.com/authorize?client_id=" + client_id + "&response_type=token" + "&redirect_uri=" + encodeURIComponent(redirect_uri) + "&scope=" + encodeURIComponent(scopes) + "&show_dialog=true"
       }
       else {
-        window.localStorage.setItem('auth', true)
-        var hash = window.location.hash.substr(1);
-        var accessToken = hash.split('=')[1].split('&')[0];
         console.log("acces_token: ", accessToken)
         fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${e.lngLat.lng.toString()},${e.lngLat.lat.toString()}.json?&access_token=${mapboxgl.accessToken}`, {
           method: "GET",
@@ -44,23 +55,36 @@ function App() {
               if (feature.place_type[0] === "country") {
                 console.log("feature: ", feature.text)
                 if (feature.text === "Turkey") {
-                  fetch("https://api.spotify.com/v1/me/player/play", {
-                    method: "PUT",
+                  fetch("https://api.spotify.com/v1/me/player/devices", {
+                    method: "GET",
                     headers: {
                       'Authorization': 'Bearer ' + accessToken
-                    },
-                    body: { 
-                      "context_uri": "spotify:album:1ez8jhd6P5k8DD0mYnfBtz"
                     }
                   })
-                  .then(res => console.log("response: ", res))
-                  .catch(err => console.log("err: ", err))
+                    .then(res => res.json())
+                    .then(result => {
+                      console.log(result.devices[0].id)
+                      fetch("https://api.spotify.com/v1/me/player/play" + "?device_id=" + result.devices[0].id, {
+                        method: "PUT",
+                        headers: {
+                          'Authorization': `Bearer ${accessToken}`,
+                          "Content-Type": "application/json",
+                          "Accept": "application/json",
+                        },
+                        body: JSON.stringify({"uris": ["spotify:track:6Mer1LOfJDd7vUbeWXODrq"]})
+                      })
+                        .then((response) => response.json())
+                        .then(data => console.log("player_result: ", data))
+                        .catch(err => console.log("player_err: ", err))
+                    }
+                    )
+                    .catch(err => console.log("device_err: ", err))
                 }
               }
             })
           })
           .catch((err) => {
-            console.log(err)
+            console.log("map_err: ", err)
           })
       }
     })
